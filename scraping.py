93c880c95843cd3ab87ca82831a6338036288daa
RENAN TEIXEIRA CARDOZO SANTANA <renanblog15@gmail.com>
2022-09-01 23:49:38 -0300

    Corrigido problema de login.

--- scraping.py
+++ scraping.py
@@ -4,6 +4,8 @@
 # pip install beautifulsoup4
 # pip install selenium
 
+from faulthandler import is_enabled
+from multiprocessing.util import is_exiting
 import time
 from urllib import request
 import requests
@@ -11,13 +13,22 @@ import pandas as pd
 import json
 
 from bs4 import BeautifulSoup
-from selenium.webdriver.firefox.service import Service
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+# funcao para verificar se elemento existe na DOM
+def is_element_present(how, what):     
+    try:         
+        driver.find_element(by=how, value=what)     
+    except NoSuchElementException as e:         
+        return False     
+    return True 
 
 # leitura dos dados do json
-
 with open("credenciais.json", encoding='utf-8') as jsonCredenciais:
     credenciais = json.load(jsonCredenciais)
 
@@ -27,47 +38,75 @@ urlTwitter = "https://twitter.com/i/flow/login"
 # instacia do firefox
 options = Options()
 options.headless = True # exibe o navegador
-service = Service(executable_path = "geckodriver-v0.31.0-win64\geckodriver.exe")
+service = Service(executable_path = "geckodriver-v0.31.0-win64\geckodriver.exe") # executavel do geckdriver
+driver = webdriver.Firefox(service = service) 
 
-driver = webdriver.Firefox(service = service)
-
+# abre pagina 
 driver.get(urlTwitter)
 
-xPathEmail = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input"
-xPathAvancar = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div"
-xPathSenha = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
-xPathEntrar = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div"
-xPathValidaNomeCelular = "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div"
+def login():
+    # atribuicao de valores primeira tela 
+    xPathEmail = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input"
+    xPathAvancar = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div"
 
-time.sleep(3)
-try:
+    # atribuicao de valores segunda tela 
+    xPathValidaNomeCelular = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input"
+    xPathAvancarUsuario = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div"
 
-    driver.find_element(By.XPATH, xPathEmail).send_keys(credenciais['email'])
-    time.sleep(2)
-    driver.find_element(By.XPATH, xPathAvancar).click()
-    time.sleep(2)
+    # atribuicao de valores terceira tela 
+    xPathSenha = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input"
+    xPathEntrar = "/html/body/div/div/div/div[1]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div"
 
-    driver.find_element(By.XPATH, xPathValidaNomeCelular).send_keys(credenciais['nome'])
-    time.sleep(2)
-    driver.find_element(By.XPATH, xPathAvancar).click()
-    time.sleep(2)
+    time.sleep(3)
+
+    # inicia login
+    try:
+
+        driver.find_element(By.XPATH, xPathEmail).send_keys(credenciais['email']) 
+        time.sleep(2)
         
-    driver.find_element(By.XPATH, xPathSenha).send_keys(credenciais['senha'])
-    time.sleep(2)
-    driver.find_element(By.XPATH, xPathEntrar).click()
-    time.sleep(2)
+        driver.find_element(By.XPATH, xPathAvancar).click() 
+        time.sleep(2)
 
+        if is_element_present('xpath', xPathValidaNomeCelular): # verifica se elemento de verificação de usuario existe 
+            driver.find_element(By.XPATH, xPathValidaNomeCelular).send_keys(credenciais['nome']) 
+            time.sleep(2)
+        
+            driver.find_element(By.XPATH, xPathAvancarUsuario).click()
+            time.sleep(2)
+            
+        driver.find_element(By.XPATH, xPathSenha).send_keys(credenciais['senha'])
+        time.sleep(2)
+        
+        driver.find_element(By.XPATH, xPathEntrar).click()
+        time.sleep(2)
+    except Exception as e:
+        driver.quit()
+        print(e)
+        return False
+    return True
 
+def search(name):
+    # atribuicao de valores
+    xPathSearchTwitter = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input"
+    XPathLasted = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[1]/div[2]/nav/div/div[2]/div/div[2]/a/div/span"
+    
+    try:
+        driver.find_element(By.XPATH, xPathSearchTwitter).send_keys(name)
+        time.sleep(2)
+        
+        driver.find_element(By.XPATH, xPathSearchTwitter).send_keys(Keys.ENTER)
+        time.sleep(2)
+        
+        driver.find_element(By.XPATH, XPathLasted).click()
+        time.sleep(2)
+    except Exception as e:
+        driver.quit()
+        print(e)
+    
 
+if login():
+    search('Bolsonaro')
 
-    teste = requests.get('https://twitter.com/home');
-    print(teste)
-
-    time.sleep(1000)
-
-    print("logado com sucesso!")
-except Exception as e:
-    driver.quit()
-    print(e)
-
+time.sleep(2)
 driver.quit()

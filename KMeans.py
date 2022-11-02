3a034c7d781a50efae9f43c918f22663c2cf2c79
Renan Santana <renan.santana@codenapp.com>
2022-11-01 22:17:15 -0300

    Correção C2

--- KMeans.py
+++ KMeans.py
@@ -1,5 +1,7 @@
 # importação de libs que serão utilizadas
 
+from click import option
+import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 
@@ -19,17 +21,33 @@ tweets_array = dataset_vetorizado.values
 # KMeans
 k_means = KMeans(n_clusters = 3).fit(tweets_array)
 
-labels = k_means.labels_
+conditions = [
+    (k_means.labels_ == 0),
+    (k_means.labels_ == 1),
+    (k_means.labels_ == 2)
+]
 
-# Fazendo mapeamento do cluster no dataset
-dataset_vetorizado_clusters = pd.DataFrame(dataset_vetorizado)
-dataset_vetorizado_clusters['Cluster'] = labels
+options  = ["negative", "neutral", "positive"]
+
+labels = np.select(conditions, options)
+
+# Fazendo mapeamento do cluster nos datasets
+dataset_vetorizado_tf_idf_clusters = pd.DataFrame(dataset_vetorizado)
+dataset_vetorizado_tf_idf_clusters['Cluster'] = labels
 
 dataset_tweets_clusters = pd.read_csv('tweets_scrapping.csv')
 dataset_tweets_clusters['Cluster'] = labels
 
+dataset_vetorizado_idf_clusters = pd.read_csv('vetorizacao_idf.csv')
+dataset_vetorizado_idf_clusters['Cluster'] = labels
+
+dataset_vetorizado_tf_clusters = pd.read_csv('vetorizacao_tf.csv')
+dataset_vetorizado_tf_clusters['Cluster'] = labels
+
 # Gerando csv dos tweets e seus respectivos cluster
-dataset_vetorizado_clusters.to_csv('tweets_vetorizados_agrupados.csv', index = True)
+dataset_vetorizado_tf_idf_clusters.to_csv('tweets_vetorizados_tf_idf_agrupados.csv', index = True)
+dataset_vetorizado_idf_clusters.to_csv('tweets_vetorizados_idf_agrupados.csv', index = True)
+dataset_vetorizado_tf_clusters.to_csv('tweets_vetorizados_tf_agrupados.csv', index = True)
 dataset_tweets_clusters.to_csv('tweets_texto_agrupados.csv', index = True)
 
 
